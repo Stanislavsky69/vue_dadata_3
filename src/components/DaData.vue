970153bad61cbe9a441db6754b299dbff7267cab
Stanislav Odnoletko <s.odnoletko@aristos.pw>
2022-02-25 20:46:12 +0300

    Вернул на синтаксис 3.2

--- src/components/DaData.vue
+++ src/components/DaData.vue
@@ -22,197 +22,180 @@
     </da-data-list>
   </div>
 </template>
-<script>
+<script setup>
 import {
   computed,
   ref,
   watch,
   toRaw,
   inject,
-  toRefs,
   onMounted,
-  defineComponent,
+  defineEmits,
+  defineProps,
 } from 'vue';
 import axios from 'axios';
 import DaDataList from './DaDataList.vue';
 import DaDataListRow from './DaDataListRow.vue';
 
-export default defineComponent({
-  components: {
-    DaDataListRow,
-    DaDataList,
+const emit = defineEmits(['update:modelValue', 'onSelected']);
+const props = defineProps({
+  modelValue: String,
+  token: {
+    type: String,
+    default: null,
   },
-  emits: ['update:modelValue', 'onSelected'],
-  props: {
-    modelValue: String,
-    token: {
-      type: String,
-      default: null,
-    },
-    type: {
-      type: String,
-      default: 'address',
-    },
-    params: {
-      type: Object,
-      default: () => {},
-    },
-    setInputValue: {
-      type: Function,
-    },
-    apiUrl: {
-      type: String,
-      default: null,
-    },
-    inputName: {
-      type: String,
-      default: 'address',
-    },
-    placeholder: {
-      type: String,
-      default: null,
-    },
-    customParams: {
-      type: Object,
-      default: () => null,
-    },
-    cssClasses: {
-      type: Object,
-      default: () => ({
-        root: 'dadata',
-        list: 'dadata__list',
-        listEmpty: 'dadata__list_empty',
-        row: 'dadata__list__row',
-      }),
-    },
+  type: {
+    type: String,
+    default: 'address',
   },
-  setup(props, { emit }) {
-    const globalOptions = inject('da-data-next-options');
-    const {
-      token, modelValue, apiUrl, type, customParams, params,
-    } = toRefs(props);
-    const tokenLocal = computed(() => {
-      if (token.value) return token.value;
-      if (globalOptions && globalOptions.token) return globalOptions.token;
-      return null;
-    });
-    const localValue = ref(modelValue.value);
-    const suggestions = ref([]);
-    const showList = ref(false);
-    const dadataDom = ref(null);
-
-    const url = computed(() => {
-      if (apiUrl.value) {
-        return apiUrl.value;
-      }
-      return `https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/${type.value}`;
-    });
-    const paramsLocal = computed(() => {
-      if (customParams.value) return customParams.value;
-      if (tokenLocal.value) {
-        return {
-          method: 'POST',
-          url: url.value,
-          headers: {
-            Authorization: `Token ${tokenLocal.value}`,
-            'content-type': 'application/json',
-            accept: 'application/json',
-          },
-          data: { ...params.value, query: localValue.value },
-        };
-      }
-      return null;
-    });
-
-    onMounted(() => {
-      document.addEventListener('click', (event) => {
-        if (dadataDom.value && !dadataDom.value.contains(event.target)) {
-          showList.value = false;
-        }
-      });
-    });
-
-    const search = () => {
-      if (!paramsLocal.value) return;
-      axios(paramsLocal.value).then((response) => {
-        if (response && response.data) {
-          if (typeof response.data.suggestions !== 'undefined') {
-            suggestions.value = response.data.suggestions;
-          } else {
-            console.error('vue-dadata-3:Свойство suggestions не найдено');
-          }
-        }
-      });
-    };
-
-    const onSelected = (data) => {
-      localValue.value = data.value;
-      if (props.setInputValue && typeof props.setInputValue === 'function') {
-        localValue.value = props.setInputValue(toRaw(data));
-      }
-      emit('onSelected', data);
-      showList.value = false;
-    };
-
-    const onFocus = () => {
-      showList.value = true;
-      if (localValue.value) {
-        search();
-      }
-    };
-
-    const prepareResults = (data, key) => {
-      let copyValue = data.value;
-      if (!key) {
-        console.error('vue-dadata-3: Укажите ключ в объекте');
-        return;
-      }
-      if (typeof key === 'object') {
-        const keyParent = Object.keys(key)[0];
-        if (!data[keyParent]) {
-          console.error(`vue-dadata-3: ${keyParent} не найден в объекте dadata`);
-          return;
-        }
-        const keyChild = key[keyParent];
-        if (!data[keyParent][keyChild]) {
-          console.error(`vue-dadata-3: ${keyChild} не найден в объекте dadata`);
-          return;
-        }
-        copyValue = data[keyParent][keyChild];
-      } else if (typeof key === 'string') {
-        if (!data[key]) {
-          console.error(`vue-dadata-3: ${key} не найден в объекте dadata`);
-          return;
-        }
-        copyValue = data[key];
-      }
-      const splitValue = localValue.value && localValue.value.split(' ');
-      if (splitValue.length > 0) {
-        splitValue.forEach((word) => {
-          copyValue = copyValue.replace(word, `<span class="highlights">${word}</span>`);
-        });
-      }
-      // eslint-disable-next-line consistent-return
-      return copyValue;
-    };
-
-    watch(() => localValue.value, (val) => {
-      emit('update:modelValue', val);
-      if (val) {
-        search();
-      } else {
-        showList.value = false;
-        suggestions.value = [];
-      }
-    });
-    return {
-      onFocus,
-      onSelected,
-      prepareResults,
-      showList,
-      localValue,
-      suggestions,
-    };
+  params: {
+    type: Object,
+    default: () => {},
+  },
+  setInputValue: {
+    type: Function,
+  },
+  apiUrl: {
+    type: String,
+    default: null,
+  },
+  inputName: {
+    type: String,
+    default: 'address',
+  },
+  placeholder: {
+    type: String,
+    default: null,
+  },
+  customParams: {
+    type: Object,
+    default: () => null,
+  },
+  cssClasses: {
+    type: Object,
+    default: () => ({
+      root: 'dadata',
+      list: 'dadata__list',
+      listEmpty: 'dadata__list_empty',
+      row: 'dadata__list__row',
+    }),
   },
 });
+
+const globalOptions = inject('da-data-next-options');
+const token = computed(() => {
+  if (props.token) return props.token;
+  if (globalOptions && globalOptions.token) return globalOptions.token;
+  return null;
+});
+const localValue = ref(props.modelValue);
+const suggestions = ref([]);
+const showList = ref(false);
+const dadataDom = ref(null);
+
+const url = computed(() => {
+  if (props.apiUrl) {
+    return props.apiUrl;
+  }
+  return `https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/${props.type}`;
+});
+const params = computed(() => {
+  if (props.customParams) return props.customParams;
+  if (token.value) {
+    return {
+      method: 'POST',
+      url: url.value,
+      headers: {
+        Authorization: `Token ${token.value}`,
+        'content-type': 'application/json',
+        accept: 'application/json',
+      },
+      data: { ...props.params, query: localValue.value },
+    };
+  }
+  return null;
+});
+
+onMounted(() => {
+  document.addEventListener('click', (event) => {
+    if (dadataDom.value && !dadataDom.value.contains(event.target)) {
+      showList.value = false;
+    }
+  });
+});
+
+const search = () => {
+  if (!params.value) return;
+  axios(params.value).then((response) => {
+    if (response && response.data) {
+      if (typeof response.data.suggestions !== 'undefined') {
+        suggestions.value = response.data.suggestions;
+      } else {
+        console.error('vue-dadata-3:Свойство suggestions не найдено');
+      }
+    }
+  });
+};
+
+const onSelected = (data) => {
+  localValue.value = data.value;
+  if (props.setInputValue && typeof props.setInputValue === 'function') {
+    localValue.value = props.setInputValue(toRaw(data));
+  }
+  emit('onSelected', data);
+  showList.value = false;
+};
+
+const onFocus = () => {
+  showList.value = true;
+  if (localValue.value) {
+    search();
+  }
+};
+
+const prepareResults = (data, key) => {
+  let copyValue = data.value;
+  if (!key) {
+    console.error('vue-dadata-3: Укажите ключ в объекте');
+    return;
+  }
+  if (typeof key === 'object') {
+    const keyParent = Object.keys(key)[0];
+    if (!data[keyParent]) {
+      console.error(`vue-dadata-3: ${keyParent} не найден в объекте dadata`);
+      return;
+    }
+    const keyChild = key[keyParent];
+    if (!data[keyParent][keyChild]) {
+      console.error(`vue-dadata-3: ${keyChild} не найден в объекте dadata`);
+      return;
+    }
+    copyValue = data[keyParent][keyChild];
+  } else if (typeof key === 'string') {
+    if (!data[key]) {
+      console.error(`vue-dadata-3: ${key} не найден в объекте dadata`);
+      return;
+    }
+    copyValue = data[key];
+  }
+  const splitValue = localValue.value && localValue.value.split(' ');
+  if (splitValue.length > 0) {
+    splitValue.forEach((word) => {
+      copyValue = copyValue.replace(word, `<span class="highlights">${word}</span>`);
+    });
+  }
+  // eslint-disable-next-line consistent-return
+  return copyValue;
+};
+
+watch(() => localValue.value, (val) => {
+  emit('update:modelValue', val);
+  if (val) {
+    search();
+  } else {
+    showList.value = false;
+    suggestions.value = [];
+  }
+});
+
 </script>
